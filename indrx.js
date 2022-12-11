2e68d2a8dba8516f16914717f8f5276a56447a84
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-12-11 05:23:54 +0200

    Create indrx.js

---
+++ indrx.js
@@ -0,0 +1,82 @@
+<!doctype html>
+<html>
+  <head>
+    <title>My Resume</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <header>
+      <h1>My Name</h1>
+      <h2>Job Title</h2>
+      <img src="profile-picture.jpg" alt="My profile picture" />
+    </header>
+    <main>
+      <section>
+        <h3>Experience</h3>
+        <article>
+          <h4>Company Name</h4>
+          <p>Brief description of role and responsibilities</p>
+        </article>
+        <article>
+          <h4>Company Name</h4>
+          <p>Brief description of role and responsibilities</p>
+        </article>
+        <article>
+          <h4>Company Name</h4>
+          <p>Brief description of role and responsibilities</p>
+        </article>
+      </section>
+      <section>
+        <h3>Education</h3>
+        <article>
+          <h4>Degree Name</h4>
+          <p>Brief description of degree and institution</p>
+        </article>
+        <article>
+          <h4>Degree Name</h4>
+          <p>Brief description of degree and institution</p>
+        </article>
+      </section>
+      <section>
+        <h3>Skills</h3>
+        <ul>
+          <li>Skill 1</li>
+          <li>Skill 2</li>
+          <li>Skill 3</li>
+          <li>Skill 4</li>
+          <li>Skill 5</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Projects</h3>
+        <article>
+          <h4>Project Name</h4>
+          <p>Brief description of project and technologies used</p>
+          <a href="https://project-website.com">Visit project website</a>
+        </article>
+        <article>
+          <h4>Project Name</h4>
+          <p>Brief description of project and technologies used</p>
+          <a href="https://project-website.com">Visit project website</a>
+        </article>
+        <article>
+          <h4>Project Name</h4>
+          <p>Brief description of project and technologies used</p>
+          <a href="https://project-website.com">Visit project website</a>
+        </article>
+      </section>
+      <section>
+        <h3>Awards</h3>
+        <ul>
+          <li>Award 1</li>
+          <li>Award 2</li>
+          <li>Award 3</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Interests</h3>
+        <ul>
+          <li>Interest 1</li>
+          <li>Interest 2</li>
+          <li>Interest 3</li>
+        </ul
